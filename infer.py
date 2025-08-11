bb953b0520fff9fe42ed06ca8389f5c2122aa11f
Rang Meng(孟让) <r_meng@zju.edu.cn>
2025-08-11 11:57:41 +0800

    set default video clip length as 65 for  VRAM saving

--- infer.py
+++ infer.py
@@ -72,11 +72,11 @@ class Config:
         # Inference parameters
         self.negative_prompt = "Gesture is bad. Gesture is unclear. Strange and twisted hands. Bad hands. Bad fingers. Unclear and blurry hands. 手部快速摆动, 手指频繁抽搐, 夸张手势, 重复机械性动作."#Unclear gestures, broken hands, more than five fingers on one hand, extra fingers, fused fingers. "# Strange body and strange trajectory. Distortion.  "
 
-        self.partial_video_length = 113
+        self.partial_video_length = 65
         self.overlap_video_length = 8
         self.neg_scale = 1.5
         self.neg_steps = 2
-        self.guidance_scale = 4.5 #4.0 ~ 6.0
+        self.guidance_scale = 4. #4.0 ~ 6.0
         self.audio_guidance_scale = 2.5 #2.0 ~ 3.0
         self.use_dynamic_cfg = True
         self.use_dynamic_acfg = True
